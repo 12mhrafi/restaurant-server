71b72375105aa1c358f7f3ccdda5b6bf2bd9ae31
12mhrafi <12mhrafi@gmail.com>
2023-11-08 23:14:00 +0600

    successfully get singleFoodData

--- index.js
+++ index.js
@@ -79,12 +79,14 @@ async function run() {
   })
 
 
-    app.get("/singleFoodDetails/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await restaurantCollection.find(query).toArray();
-      res.send(result);
-    })
+  app.get("/singleFoodDetails/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await restaurantCollection.find(query).toArray();
+    res.send(result);
+  })
+
+ 
 
     // { _id, image, FoodName, price, category, quantity, description } 
 
